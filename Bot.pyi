ddb9e4dc3a84ccf9d999f3e7ff14dd0240266318
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-04-17 20:31:44 -0700

    import discord, update_stats, messages, member_join

---
+++ Bot.pyi
@@ -0,0 +1,49 @@
+import discord
+import time
+import asyncio
+#id = 599461294428520490
+messages = joined = 0
+
+client = discord.Client()
+
+async def update_stats():
+    await client.wait_until_ready()
+    global messages, joined
+
+    while not client.is_closed():
+        try:
+            with open("stats", "a") as f:
+                f.write(f"""Time: {int (time.time())}, Messages: {messages}, Members Joined: {joined}\n""")
+            messages = 0
+            joined = 0
+            await asyncio.sleep(5)
+        except Exception as e:
+            print(e)
+            await asyncio.sleep(5)
+
+@client.event
+async def on_member_join(member):
+    global joined
+    joined += 1
+    for channel in member.server.channels:
+        if str(channel) == "welcome":
+            await client.send_message(f"""Welcome to Space Breakers {member.mention}""")
+
+@client.event
+async def on_message(message):
+    global messages
+    messages += 1
+    id = client.get_guild(599461294428520490)
+    channels = ("test")
+    valid_users = ("thefluffyoshi#6195", "friend#5869")
+
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!hello") != -1:
+            await message.channel.send("Hi")
+        elif message.content == "!users":
+            await message.channel.send(f"""# of Members: {id.member_count}""")
+#    else:
+#        print(f"""User: {message.author} tried to do command {message.content}, in channel {message.channel}""")
+
+client.loop.create_task(update_stats())
+client.run("Njk0NDE4MDAxOTM5OTIyOTc2.Xpf1eA.OnXLj7lVjycEkhrerDO4v36f27s")

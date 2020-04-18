33a1520c878fd211e2762056962c808a90041022
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-04-17 20:58:21 -0700

    commented out update_stats, commented out else, added adria

--- Bot.pyi
+++ Bot.pyi
@@ -2,40 +2,41 @@ import discord
 import time
 import asyncio
 #id = 599461294428520490
-messages = joined = 0
+#messages = joined = 0
 
 client = discord.Client()
 
-async def update_stats():
-    await client.wait_until_ready()
-    global messages, joined
+#async def update_stats():
+    #await client.wait_until_ready()
+    #global messages, joined
 
-    while not client.is_closed():
-        try:
-            with open("stats", "a") as f:
-                f.write(f"""Time: {int (time.time())}, Messages: {messages}, Members Joined: {joined}\n""")
-            messages = 0
-            joined = 0
-            await asyncio.sleep(5)
-        except Exception as e:
-            print(e)
-            await asyncio.sleep(5)
+    #while not client.is_closed():
+        #try:
+            #with open("stats.txt", "a") as f:
+                #f.write(f"""Time: {int (time.time())}, Messages: {messages}, Members Joined: {joined}\n""")
+            #messages = 0
+            #joined = 0
+            #await asyncio.sleep(5)
+        #except Exception as e:
+            #print(e)
+            #await asyncio.sleep(5)
 
 @client.event
 async def on_member_join(member):
-    global joined
-    joined += 1
-    for channel in member.server.channels:
+    #global joined
+    #joined += 1
+
+    for channel in member.guild.channels:
         if str(channel) == "welcome":
             await client.send_message(f"""Welcome to Space Breakers {member.mention}""")
 
 @client.event
 async def on_message(message):
-    global messages
-    messages += 1
+    #global messages
+    #messages += 1
     id = client.get_guild(599461294428520490)
-    channels = ("test")
-    valid_users = ("thefluffyoshi#6195", "friend#5869")
+    channels = ["test", "bot-spam", "bot-spam-2", "test-2", "welcome"]
+    valid_users = ["thefluffyoshi#6195", "friend#5869", "adria#9936"]
 
     if str(message.channel) in channels and str(message.author) in valid_users:
         if message.content.find("!hello") != -1:
@@ -45,5 +46,5 @@ async def on_message(message):
 #    else:
 #        print(f"""User: {message.author} tried to do command {message.content}, in channel {message.channel}""")
 
-client.loop.create_task(update_stats())
+#client.loop.create_task(update_stats())
 client.run("Njk0NDE4MDAxOTM5OTIyOTc2.Xpf1eA.OnXLj7lVjycEkhrerDO4v36f27s")
